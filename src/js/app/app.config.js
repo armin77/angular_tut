3ea770f6692dc6c2a08af0bd49da47248b042f2a
Armin Schweitzer <armin.schweitzer@gmail.com>
2016-11-15 21:30:44 +0100

    adding first routes html5Mode requires a base-tag!

--- src/js/app/app.config.js
+++ src/js/app/app.config.js
@@ -1,7 +1,25 @@
 'use strict';
 
 angular.module('try').
-  config(function() {
+  config(function($locationProvider, $routeProvider) {
     console.log('CONFIG called');
     
+    $locationProvider.html5Mode({
+      enabled: true
+    });
+
+    $routeProvider
+      .when('/', {
+        template: 'Home<blog-list></blog-list>',
+      })
+      .when('/blog/1', {
+        template: '<h1>Page 1</h1>',
+      })
+      .when('/blog/2', {
+        template: '<blog-list></blog-list>',
+      })
+      .otherwise({
+        template: '<h1>Not found</h1>',
+      })
+
   });
