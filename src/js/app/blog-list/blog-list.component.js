33a1a25d3f23ac82db16905e1f9d5c8b2bd0ecfb
Armin Schweitzer <armin.schweitzer@gmail.com>
2016-11-15 20:42:53 +0100

    changing scope within clickHandler

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -6,7 +6,11 @@ angular.module('blogList').
         
         $scope.sometext = "Just a little text....";
 
+        $scope.clicks = 0;
         $scope.someClickHandler = function () {
             console.log('button Clicked...');
+
+            $scope.clicks++;
+            $scope.sometext = 'Button clicked ' + $scope.clicks + ' times!';
         }
     });
