7619e48f9016f00e2e867652af085f9170e1f496
Armin Schweitzer <armin.schweitzer@gmail.com>
2016-11-15 20:56:28 +0100

    externalize template into separate file

--- src/js/app/app.config.js
+++ src/js/app/app.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('try').
-    config(function() {
-        console.log('CONFIG called');
-        
-    });
+  config(function() {
+    console.log('CONFIG called');
+    
+  });
--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -1,23 +1,16 @@
 'use strict';
 
 angular.module('blogList').
-    component('blogList', {
-        template: `
-        <div>
-            <h1>{{sometext}}</h1>
+  component('blogList', {
+    templateUrl: 'templates/blog-list.html',
+    controller: function ($scope) {
+      $scope.sometext = "Just a little text....";
 
-            <button ng-click='someClickHandler()'>Click me...</button>
-        </div>
-        `,
-        controller: function ($scope) {
-            $scope.sometext = "Just a little text....";
-
-            $scope.clicks = 0;
-            $scope.someClickHandler = function () {
-                console.log('button Clicked...');
-
-                $scope.clicks++;
-                $scope.sometext = 'Button clicked ' + $scope.clicks + ' times!';
-            }
-        }
-    });
+      $scope.clicks = 0;
+      $scope.someClickHandler = function () {
+        console.log('button Clicked...');
+        $scope.clicks++;
+        $scope.sometext = 'Button clicked ' + $scope.clicks + ' times!';
+      }
+    }
+  });
