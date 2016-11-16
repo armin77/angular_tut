6209bc653fa516a4acab907e51d8fd7379fc749f
Armin Schweitzer <armin.schweitzer@gmail.com>
2016-11-16 20:03:34 +0100

    rewrote directive to be a confirmable anker-tag

--- src/js/app/utils/confirm-click/confirm-click.directive.js
+++ src/js/app/utils/confirm-click/confirm-click.directive.js
@@ -2,12 +2,26 @@
     'use strict';
 
     angular.module('confirmClick')
-      .directive('confirmClick', () => {
+      .directive('confirmClick', ($rootScope, $location) => {
         return {
+          scope: {
+            message: '@message',
+            post: '=post',
+          },
+          restrict: 'E',
+          template: '<a ng-href="#">{{post.title}}</a>',
           link: function (scope, element, attr) {
-            console.log(scope);
-            console.log(element);
-            console.log(attr);
+            var msg = scope.message || 'Are you sure?';
+
+            element.bind('click', function (e) {
+              if (window.confirm(msg)) {
+
+                // make sure to get this executed!
+                $rootScope.$apply(function () { 
+                  $location.path('/blog/' + scope.post.id)
+                });
+              }
+            })
           }
         }
       });
