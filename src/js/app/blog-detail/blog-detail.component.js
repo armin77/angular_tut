63eb17407b39193fee34e6fed25cfcab2dbf4405
Armin Schweitzer <armin.schweitzer@gmail.com>
2016-11-16 09:48:53 +0100

    added detail-view

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -5,6 +5,28 @@ angular.module('blogDetail').
     templateUrl: 'templates/blog-detail.html',
     controller: function ($scope, $routeParams) {
 
-      $scope.title = "Blog " + $routeParams.id;
+      console.log($routeParams);
+
+      var blogItems = [
+        {title: 'someTitle', id: 1, description: 'this is the first description'},
+        {title: 'someOtherTitle', id: 2, description: 'this is the second description'},
+        {title: 'randomTitle', id: 3, description: 'this is the third description'},
+        {title: '4th-Title', id: 4, description: 'this is the forth description'},
+        {title: '5th-Title', id: 5, description: 'this is the fifth description'},
+        {title: 'lastTitle', id: 6, description: 'this is the sixth description'},
+      ];
+
+      $scope.postFound = false;
+
+      _.each(blogItems, post => {
+        if (post.id == $routeParams.id) {
+          $scope.postFound = true;
+          $scope.post = post;
+        }
+      });
+
+      if (!$scope.postFound) {
+        $scope.post = null;
+      }
     }
   });
